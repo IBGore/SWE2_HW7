cdde41552351fe97123220de3583aea469bd756e
Bradley Gore <gorebradleyi@gmail.com>
2021-03-06 20:02:32 -0800

    Wrote Second test, Checks if the correct statment is printed "n is a leap year!" or "n is not a leap year!"

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -1,6 +1,9 @@
 import unittest
 import LeapYear as LY
 
+from io import StringIO  # Python 3
+import sys
+
 class TestCase(unittest.TestCase):
     #Check if leap year is determined correctly
     def test_IsLeap1(self):
@@ -14,6 +17,39 @@ class TestCase(unittest.TestCase):
 
     def test_IsLeap400(self):
         self.assertEqual(LY.isLeapYear(400), True)
+
+
+    def test_Printed_1(self):
+        # create a temporary stream to hold STDOut
+        temp_out = StringIO()
+
+        #redirect stdout (Print statments) to temporary stram
+        sys.stdout = temp_out
+        
+        #run printLeapYear
+        LY.printIsYear(1)
+
+        #check correct statement printed
+        self.assertEqual(temp_out.getvalue(), "1 is not a leap year!\n")
+
+        #restore stdout location
+        sys.stdout = sys.__stdout__
+
+    def test_Printed_4(self):
+        # create a temporary stream to hold STDOut
+        temp_out = StringIO()
+
+        #redirect stdout (Print statments) to temporary stram
+        sys.stdout = temp_out
+        
+        #run printLeapYear
+        LY.printIsYear(4)
+
+        #check correct statement printed
+        self.assertEqual(temp_out.getvalue(), "4 is a leap year!\n")
+
+        #restore stdout location
+        sys.stdout = sys.__stdout__
 
 
 if __name__ == '__main__':
