92e7e3d6883716de78e25749e926aa08405b4e9c
Bradley Gore <gorebradleyi@gmail.com>
2021-03-06 19:24:11 -0800

    Wrote Second test, {Check correct lines printed to stdout}

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -1,38 +1,132 @@
 import unittest
+import pytest
 import FizzBuzz as FB
+
+from io import StringIO  # Python 3
+import sys
+
+
+
+# Replace default stdout (terminal) with our stream
+
 
 
 class TestCase(unittest.TestCase):
-    def test_fb1(self):
+    #Check correct strings generaged
+    def test_Num1(self):
         self.assertEqual(FB.toStr(1), "1")
 
-    def test_fb2(self):
+    def test_Num7(self):
         self.assertEqual(FB.toStr(7), "7")
 
-    def test_fb3(self):
+    def test_Num3(self):
         self.assertEqual(FB.toStr(3), "Fizz")
 
-    def test_fb4(self):
+    def test_Num5(self):
         self.assertEqual(FB.toStr(5), "Buzz")
 
-    def test_fb5(self):
+    def test_Num15(self):
         self.assertEqual(FB.toStr(15), "FizzBuzz")
 
 
-    
+    #Check correct number of strings printed
+    def test_Printed_100_Lines(self):
+        # create a temporary stream to hold STDOut
+        temp_out = StringIO()
 
-
-
-    # def test_fb#(self):
-    #     with self.assertRaises(AssertionError):
-    #         #function that errors
-
-
+        #redirect stdout (Print statments) to temporary stram
+        sys.stdout = temp_out
         
+        #run printFizzBuzz to print 100 lines
+        FB.printFizzBuzz()
 
+        #check 100 lines have been printed
+        self.assertEqual(temp_out.getvalue().count("\n"), 100)
+
+        #restore stdout location
+        sys.stdout = sys.__stdout__
+
+
+    #Check correct strings are printed
+    def test_Printed_Num(self):
+        # create a temporary stream to hold STDOut
+        temp_out = StringIO()
+
+        #redirect stdout (Print statments) to temporary stram
+        sys.stdout = temp_out
+        
+        #run printFizzBuzz to print 100 lines
+        FB.printFizzBuzz()
+
+        #creat an array of lines
+        lines = temp_out.getvalue().split("\n")
+
+        #check line 1 == "1"
+        self.assertEqual(lines[0], "1")
+
+        #restore stdout location
+        sys.stdout = sys.__stdout__
+
+    def test_Printed_Fizz(self):
+        # create a temporary stream to hold STDOut
+        temp_out = StringIO()
+
+        #redirect stdout (Print statments) to temporary stram
+        sys.stdout = temp_out
+        
+        #run printFizzBuzz to print 100 lines
+        FB.printFizzBuzz()
+
+        #creat an array of lines
+        lines = temp_out.getvalue().split("\n")
+
+        #check line 3 == "Fizz"
+        self.assertEqual(lines[2], "Fizz")
+
+        #restore stdout location
+        sys.stdout = sys.__stdout__
+
+    def test_Printed_Buzz(self):
+        # create a temporary stream to hold STDOut
+        temp_out = StringIO()
+
+        #redirect stdout (Print statments) to temporary stram
+        sys.stdout = temp_out
+        
+        #run printFizzBuzz to print 100 lines
+        FB.printFizzBuzz()
+
+        #creat an array of lines
+        lines = temp_out.getvalue().split("\n")
+
+        #check line 5 == "Buzz"
+        self.assertEqual(lines[4], "Buzz")
+
+        #restore stdout location
+        sys.stdout = sys.__stdout__
+
+    def test_Printed_FizzBuzz(self):
+        # create a temporary stream to hold STDOut
+        temp_out = StringIO()
+
+        #redirect stdout (Print statments) to temporary stram
+        sys.stdout = temp_out
+        
+        #run printFizzBuzz to print 100 lines
+        FB.printFizzBuzz()
+
+        #creat an array of lines
+        lines = temp_out.getvalue().split("\n")
+
+        #check line 15 == "FizzBuzz"
+        self.assertEqual(lines[14], "FizzBuzz")
+
+        #restore stdout location
+        sys.stdout = sys.__stdout__
     
 
 
+    
 if __name__ == '__main__':
     unittest.main(verbosity=2)
 
