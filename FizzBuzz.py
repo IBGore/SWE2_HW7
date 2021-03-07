d28d266d8f3f172110ed9dec492a34f575be5637
Bradley Gore <gorebradleyi@gmail.com>
2021-03-06 18:42:37 -0800

    Implemented toStr method {prints num as string. Checks if divisable by 3 or 5}

---
+++ FizzBuzz.py
@@ -0,0 +1,8 @@
+def toStr(num):
+    if(num % 3 == 0 and num % 5 == 0):
+        return "FizzBuzz"
+    if(num % 3 == 0):
+        return "Fizz"
+    if(num % 5 == 0):
+        return "Buzz"
+    return str(num)
